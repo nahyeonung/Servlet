ccda71721b4a9b8dfcf1bf943f3eb70be942f06e
nahyeonung <ntweety@naver.com>
2025-07-30 22:02:29 +0900

    HttpServeltResponse 사용법

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -15,8 +15,6 @@ public class RequestHeaderServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.service(request, response);
-
         printStartLine(request);
         printHeaders(request);
         printHeaderUtils(request);
