7153c6de92af31479d632ee8b9cee06465b7365a
nahyeonung <ntweety@naver.com>
2025-12-08 17:27:01 +0900

    rebase

--- src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -1,2 +1,22 @@
-package hello.servlet.web.frontcontroller.v4.controller;public class MemberListControllerV4 {
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepositary;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+    private MemberRepositary memberRepositary = MemberRepositary.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+
+        List<Member> members = memberRepositary.findAll();
+        model.put("members", members);
+
+        return "members";
+    }
 }
